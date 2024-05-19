2dcc3cfbdbd5aab2aa6e02a0dd0d7b40a935f02f
kyleburgin1 <kyleburginn@gmail.com>
2024-05-19 13:10:52 +1000

    test push

---
+++ javascript.js
@@ -0,0 +1,20 @@
+let num1;
+let num2;
+let operator;
+
+function add(){
+   num1 + num2;
+};
+
+function multiply(){
+    num1 * num2;
+};
+
+function divide(){
+    num1 / num2;
+};
+
+function subtract(){
+    num1 - num2;
+};
+
